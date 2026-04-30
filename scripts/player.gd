8156ede82319da74b91ade69312bf067c29485fc
LynnePlex <38146775+BliniMango@users.noreply.github.com>
2026-04-30 16:26:16 -0500

    basic player fps
    implemented basic player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,40 @@
-extends Node3D
+extends CharacterBody3D
+
+@onready var camera_3d = $Camera3D
 
 
-# Called when the node enters the scene tree for the first time.
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const CAMERA_SENS = 0.003
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _ready():
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+func _input(event):
+	if event.is_action_pressed("quit"): get_tree().quit()
+	
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x * CAMERA_SENS
+		rotation.x -= event.relative.y * CAMERA_SENS
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var input_dir = Input.get_vector("move_left","move_right","move_forward","move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
